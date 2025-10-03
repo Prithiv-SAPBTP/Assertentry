0058bfd67d77fbcd5fe876b5600285ff88a9641c
Prithiv G <prithiv.g@laderatechnology.com>
2025-10-03 10:13:51 +0000

    commit

--- webapp/controller/PostPage.controller.js
+++ webapp/controller/PostPage.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
+    "sap/m/MessageToast",        
     "sap/m/MessageBox"
 ], (Controller,MessageToast,MessageBox) => {
     "use strict";
