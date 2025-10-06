b2e1a7dbc875246ce833c4a2a855c3efb06f4af9
Prithiv G <prithiv.g@laderatechnology.com>
2025-10-06 07:39:15 +0000

    recent

--- webapp/controller/PostPage.controller.js
+++ webapp/controller/PostPage.controller.js
@@ -7,8 +7,31 @@ sap.ui.define([
 
     return Controller.extend("assertentry.controller.PostPage", {
         onInit: function() {
+            this.getView().setModel(this.getOwnerComponent().getModel("odata"));
         },
+        onPressGo: function(){
+            var oView = this.getView();
+            var oDataModel = this.getOwnerComponent().getModel();
+            var ojsonModel = new sap.ui.model.json.JSONModel();
+            var oPersonId = oView.byId("PersonId").getValue();
 
+            var sPath = "/per_pay_dataSet('" + oPersonId + "')";
+
+    oDataModel.read(sPath, {
+        success: function (oData) {
+            ojsonModel.setData(oData);
+            this.getView().setModel(ojsonModel, "oModel");
+
+            sap.m.MessageToast.show("Data loaded for Person ID: " + oPersonId);
+        }.bind(this),
+        error: function (oError) {
+            sap.m.MessageToast.show("Error fetching data for Person ID: " + oPersonId);
+            console.error(oError);
+        }.bind(this)
+    });
+
+            
+        }
 
     });
 });
