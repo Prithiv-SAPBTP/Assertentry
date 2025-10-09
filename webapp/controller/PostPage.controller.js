2ec1ac80f9ec8e9940157487812f9d012fe159d0
Prithiv G <prithiv.g@laderatechnology.com>
2025-10-09 12:40:29 +0000

    new commit

--- webapp/controller/PostPage.controller.js
+++ webapp/controller/PostPage.controller.js
@@ -12,29 +12,6 @@ sap.ui.define([
 const oEmptyModel = new JSONModel([]);
             this.getView().setModel(oEmptyModel, "oModel");
         },
-    //     onPressGo: function(){
-    //         var oView = this.getView();
-    //         var oDataModel = this.getOwnerComponent().getModel();
-    //         var ojsonModel = new sap.ui.model.json.JSONModel();
-    //         var oPersonId = oView.byId("PersonId").getValue();
-
-    //         var sPath = "/per_pay_dataSet('" + oPersonId + "')";
-
-    // oDataModel.read(sPath, {
-    //     success: function (oData) {
-    //         ojsonModel.setData(oData);
-    //         this.getView().setModel(ojsonModel, "oModel");
-
-    //         sap.m.MessageToast.show("Data loaded for Person ID: " + oPersonId);
-    //     }.bind(this),
-    //     error: function (oError) {
-    //         sap.m.MessageToast.show("Error fetching data for Person ID: " + oPersonId);
-    //         console.error(oError);
-    //     }.bind(this)
-    // });
-
-            
-    //     }
 
       onPressGo: function () {
             const oView = this.getView();
@@ -67,9 +44,100 @@ const oEmptyModel = new JSONModel([]);
                 }.bind(this)
             });
         },
-onPressGetapprove:function(){
 
+onPressGetapprove: async function () {
+    // const oTable = this.getView().byId("personnelTable");
+    // const aSelectedItems = oTable.getSelectedItems();
+
+    // if (aSelectedItems.length === 0) {
+    //     sap.m.MessageBox.warning("Please select at least one record to get approval.");
+    //     return;
+    // }
+
+    // const oCtx = aSelectedItems[0].getBindingContext("oModel").getObject();
+
+    // //  const oPayload = {
+    // //     Pernr: oCtx.Pernr,
+    // //     Username: oCtx.Username,
+    // //     MailId: oCtx.MailId,
+    // //     StartDate: oCtx.Begda,
+    // //     EndDate: oCtx.Endda
+    // // };
+
+    // try {
+    //     const sUrl = "https://spa-api-gateway-bpi-us-prod.cfapps.us10.hana.ondemand.com/workflow/rest/v1/workflow-instances?environmentId=tutorial";
+
+    //     const response = await fetch(sUrl, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Authorization": "Bearer " + "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYjc0NDlmZTR0cmlhbC5hdXRoZW50aWNhdGlvbi51czEwLmhhbmEub25kZW1hbmQuY29tL3Rva2VuX2tleXMiLCJraWQiOiJkZWZhdWx0LWp3dC1rZXktZmEwY2M1YjNmMyIsInR5cCI6IkpXVCIsImppZCI6ICJtVmJPWEY4OFI1QjEzem1sTDliUzFQOXVuOXJKR1BBRnJORkxkTkdaQUw4PSJ9.eyJqdGkiOiIyNjAyMGQ2YmE4MTU0ZDZjOWY0Y2QyZWE3ZGJkMTRjYSIsImV4dF9hdHRyIjp7ImVuaGFuY2VyIjoiWFNVQUEiLCJzdWJhY2NvdW50aWQiOiI4ZDVkOWQzOS0yYzIxLTQzNjMtYTZlOC1kMTU2NzNhMGQ0YjMiLCJ6ZG4iOiJiNzQ0OWZlNHRyaWFsIiwic2VydmljZWluc3RhbmNlaWQiOiJiOWY1N2Q4My1jMDM0LTRhNmUtYTE5MS1jNmQ3YTIyOTgwNjUifSwic3ViIjoic2ItYjlmNTdkODMtYzAzNC00YTZlLWExOTEtYzZkN2EyMjk4MDY1IWI0OTU1Mzd8eHN1YWEhYjQ5MzkwIiwiYXV0aG9yaXRpZXMiOlsidWFhLnJlc291cmNlIl0sInNjb3BlIjpbInVhYS5yZXNvdXJjZSJdLCJjbGllbnRfaWQiOiJzYi1iOWY1N2Q4My1jMDM0LTRhNmUtYTE5MS1jNmQ3YTIyOTgwNjUhYjQ5NTUzN3x4c3VhYSFiNDkzOTAiLCJjaWQiOiJzYi1iOWY1N2Q4My1jMDM0LTRhNmUtYTE5MS1jNmQ3YTIyOTgwNjUhYjQ5NTUzN3x4c3VhYSFiNDkzOTAiLCJhenAiOiJzYi1iOWY1N2Q4My1jMDM0LTRhNmUtYTE5MS1jNmQ3YTIyOTgwNjUhYjQ5NTUzN3x4c3VhYSFiNDkzOTAiLCJncmFudF90eXBlIjoiY2xpZW50X2NyZWRlbnRpYWxzIiwicmV2X3NpZyI6IjhjMjk1MjQ5IiwiaWF0IjoxNzYwMDAyODQ3LCJleHAiOjE3NjAwNDYwNDcsImlzcyI6Imh0dHBzOi8vYjc0NDlmZTR0cmlhbC5hdXRoZW50aWNhdGlvbi51czEwLmhhbmEub25kZW1hbmQuY29tL29hdXRoL3Rva2VuIiwiemlkIjoiOGQ1ZDlkMzktMmMyMS00MzYzLWE2ZTgtZDE1NjczYTBkNGIzIiwiYXVkIjpbInVhYSIsInNiLWI5ZjU3ZDgzLWMwMzQtNGE2ZS1hMTkxLWM2ZDdhMjI5ODA2NSFiNDk1NTM3fHhzdWFhIWI0OTM5MCJdfQ.O9e6MsEpj5Q13D3x0ZkDhnSf-lrAyfOlwfnI2tY5AJGF40Awe8ikILUZ1yMLjd4xM18krQdhnUInip6k1bBDqjZKgUo3vOVN-nI8bW4uajrie59VHmSKW1heEoVz4H4RAnSFUJfF9XRvhAJk9VukAUtiLILAMeJKH8ROi9hncRv1l3-_C-wTJh4cFEfywMDer0VPHhs7L-Ul7YZEdg0iKFjzm4_XwIj-lg1IM85gITZTTtqfMKc6dzKNTZqCljwRfrrvCuuiH8PYLd-moI32K37xu9OqkMgir-732wmqzDWQVUsxPGreXEoVT7Cp03b5_Ty5sZApBJCFK3UhmcLP0Q"   
+    //         },
+    //     body: JSON.stringify({
+    //         "definitionId": "us10.b7449fe4trial.postingtofateam.api_trigger",
+    //         "context": {
+    //             "pernr":oCtx.Pernr ,
+    //             "username": oCtx.Username,
+    //             "mailid": oCtx.MailId
+    //     }
+    //         })
+    //     });
+
+    //     if (response.ok) {
+    //         sap.m.MessageToast.show("Approval workflow triggered successfully!");
+    //     } else {
+    //         const errText = await response.text();
+    //         sap.m.MessageBox.error("Failed to trigger workflow: " + errText);
+    //     }
+    // } catch (err) {
+    //     console.log(err);
+    //     sap.m.MessageBox.error("Error calling workflow API: " + err.message);
+    // }
+
+     const oTable = this.getView().byId("personnelTable");
+            const aSelectedItems = oTable.getSelectedItems();
+
+            if (aSelectedItems.length === 0) {
+                MessageBox.warning("Please select at least one record to get approval.");
+                return;
+            }
+
+            const oCtx = aSelectedItems[0].getBindingContext("oModel").getObject();
+
+            try {
+                const sUrl = "/workflow-api/workflow/rest/v1/workflow-instances?environmentId=tutorial";
+
+                const oPayload = {
+                    definitionId: "us10.b7449fe4trial.postingtofateam.api_trigger",
+                    context: {
+                        pernr: oCtx.Pernr,
+                        username: oCtx.Username,
+                        mailid: oCtx.MailId
+                    }
+                };
+
+                const response = await fetch(sUrl, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(oPayload)
+                });
+
+                if (response.ok) {
+                    MessageToast.show("Approval workflow triggered successfully!");
+                } else {
+                    const errText = await response.text();
+                    MessageBox.error("Failed to trigger workflow: " + errText);
+                }
+
+            } catch (err) {
+                console.error("Workflow API error:", err);
+                MessageBox.error("Error calling workflow API: " + err.message);
+            }
+        
 },
+
 
 //     onPressGo: function () {
 //     const oView = this.getView();
